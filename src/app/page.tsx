3a6a91dc51aebf8ba6b3e001ff3c1375b973aedc
Joel Ekeng <joelekeng23@gmail.com>
2024-12-27 23:13:50 +0000

    Added suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,16 +5,16 @@ import { Suspense } from 'react'
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import { Deals} from "@/components/LandingPage";
-import Search from "@/components/Search";
+// import Search from "@/components/Search";
 
 export default function Home() {
   return (
     <div className="pb-20 gap-16 font-[family-name:var(--font-geist-sans)]">
       <Header />
       <div className="px-10">
-      <Suspense fallback={<div>Loading...</div>}>
+      {/* <Suspense fallback={<div>Loading...</div>}>
       <Search />
-     </Suspense>
+     </Suspense> */}
       <Deals/>
       </div>
       <Footer/>
